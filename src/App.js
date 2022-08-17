d786bb339e64f553040556679bdea01735779c47
ibrahim sharawy <85072157+sh333rawy@users.noreply.github.com>
2022-08-17 16:49:49 +0200

    Add Checkout component

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import { Routes, Route } from "react-router-dom";
 import ProductDetail from "./components/ProductDetail";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
     <>
       <Header />
-      
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/about" element={<About/>} />
-        <Route exact path="/contact" element={<Contact/>} />
-        <Route exact path="/products" element={<Product />} />
-        <Route exact path="/products/:id" element={<ProductDetail/>} />
 
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/products" element={<Product />} />
+        <Route exact path="/products/:id" element={<ProductDetail />} />
+        <Route exact path="/Cart" element={<Cart />} />
+        <Route exact path="/Checkout" element={<Checkout />} />
       </Routes>
       <Footer />
     </>
